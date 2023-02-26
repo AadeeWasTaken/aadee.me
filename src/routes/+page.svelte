5f64d4e340a89070d31cd4456c452fce1dd51c52
Aadee <aadeetyapatil@gmail.com>
2023-02-26 00:02:04 -0500

    removed repetition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,31 @@
 	let githubHover: boolean = false;
 	let linkedinHover: boolean = false;
 
-	let twitterHovered = () => (twitterHover = true);
-	let twitterUnhovered = () => (twitterHover = false);
-
-	let githubHovered = () => (githubHover = true);
-	let githubUnhovered = () => (githubHover = false);
-
-	let linkedinHovered = () => (linkedinHover = true);
-	let linkedinUnhovered = () => (linkedinHover = false);
+	const socialMedia: {
+		name: string;
+		link: string;
+		hover: () => boolean;
+		unhover: () => boolean;
+	}[] = [
+		{
+			name: 'twitter',
+			link: 'https://twitter.com/AadeeWasTaken',
+			hover: () => (twitterHover = true),
+			unhover: () => (twitterHover = false)
+		},
+		{
+			name: 'github',
+			link: 'https://github.com/AadeeWasTaken',
+			hover: () => (githubHover = true),
+			unhover: () => (githubHover = false)
+		},
+		{
+			name: 'linkedin',
+			link: 'https://www.linkedin.com/in/aadee/',
+			hover: () => (linkedinHover = true),
+			unhover: () => (linkedinHover = false)
+		}
+	];
 </script>
 
 <svelte:head>
@@ -20,25 +37,9 @@
 <div class="h-screen w-screen font-display text-white">
 	<div class="h-screen flex flex-col space-y-20 justify-center items-center">
 		<div class="flex justify-center items-center">
-			{#if twitterHover}
+			{#if twitterHover || githubHover || linkedinHover}
 				<ul class="flex space-x-14 text-9xl cursor-default">
-					{#each 'TWITTER' as letter}
-						<li class="bg-white text-black">
-							{letter}
-						</li>
-					{/each}
-				</ul>
-			{:else if githubHover}
-				<ul class="flex space-x-14 text-9xl cursor-default">
-					{#each 'GITHUB' as letter}
-						<li class="bg-white text-black">
-							{letter}
-						</li>
-					{/each}
-				</ul>
-			{:else if linkedinHover}
-				<ul class="flex space-x-14 text-9xl cursor-default">
-					{#each 'LINKEDIN' as letter}
+					{#each twitterHover ? 'TWITTER' : githubHover ? 'GITHUB' : 'LINKEDIN' as letter}
 						<li class="bg-white text-black">
 							{letter}
 						</li>
@@ -58,36 +59,18 @@
 		</div>
 		<div class="flex justify-center">
 			<ul class="flex space-x-14">
-				<li>
-					<a
-						href="https://twitter.com/AadeeWasTaken"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="underline underline-offset-4 p-2 hover:bg-white hover:text-black hover:no-underline"
-						on:mouseenter={() => twitterHovered()}
-						on:mouseleave={() => twitterUnhovered()}>twitter</a
-					>
-				</li>
-				<li>
-					<a
-						href="https://github.com/AadeeWasTaken"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="underline underline-offset-4 p-2 hover:bg-white hover:text-black hover:no-underline"
-						on:mouseenter={() => githubHovered()}
-						on:mouseleave={() => githubUnhovered()}>github</a
-					>
-				</li>
-				<li>
-					<a
-						href="https://www.linkedin.com/in/aadee/"
-						target="_blank"
-						rel="noopener noreferrer"
-						class="underline underline-offset-4 p-2 hover:bg-white hover:text-black hover:no-underline"
-						on:mouseenter={() => linkedinHovered()}
-						on:mouseleave={() => linkedinUnhovered()}>linkedin</a
-					>
-				</li>
+				{#each socialMedia as social}
+					<li>
+						<a
+							href={social.link}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="underline underline-offset-4 p-2 hover:bg-white hover:text-black hover:no-underline"
+							on:mouseenter={() => social.hover()}
+							on:mouseleave={() => social.unhover()}>{social.name}</a
+						>
+					</li>
+				{/each}
 			</ul>
 		</div>
 	</div>
